586f4dcf57093ea6f5556f735a5258d617347cfa
Nikita Chernov <ns4ern@gmail.com>
2021-12-24 04:03:26 +0300

    верстка добавлена

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section className='header'>
+        <h3 className='header__title'>To Do List</h3>
+      </section>
+      <section className='content'>
+        <div className='tasks'>
+          <div className='tasks__div' >
+            <input className='tasks__input' placeholder='Введите текст' />
+            <button className='task__btn tasks__send'>Send</button>
+          </div>
+          <div className='task'>
+            <p className='task__text'>Текст задачи</p>
+            <button className='task__btn task__btn_edit' />
+            <button className='task__btn tast__btn_delete'>Delete</button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
