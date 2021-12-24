e7d150778a41578b007aa1d07e1374926e46a9f2
Nikita Chernov <ns4ern@gmail.com>
2021-12-24 05:22:32 +0300

    add card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
+import React, { useState } from 'react';
 import './App.css';
+
 function App() {
+
+  const [value, setValue] = useState('');
+  const [taskText, setTaskText] = useState('');
+  const [check, setCheck] = useState();
+  const [card, setCard] = useState([]);
+  
+  let count = 0;
+  
+  function onChange (e) {
+     setValue(e.target.value)  
+  }
+
+  function sendBtn (evt) {
+    evt.preventDefault();
+    setTaskText(value);
+    setCard(card)
+  }
+
+  function editBtn (evt) {
+    evt.preventDefault();
+    setValue(taskText);
+  }
+  function toggleCheckBtn (evt) {
+    evt.preventDefault();
+    setCheck(!check);
+  }
+
+  function addTask () {
+    return (
+      <>
+        <div className='task'>
+          <p className={`task__text ${check ? 'check' : ''}`}>{taskText}</p>
+          <button type='button' className='task__btn task__btn_edit' onClick={editBtn}/>
+          <button type='button' className='task__btn task__btn_completed' onClick={toggleCheckBtn}/>
+          <button type='reset' className='task__btn tast__btn_delete' />
+        </div>
+      </>
+    )
+  }
   return (
     <div>
       <section className='header'>
@@ -8,15 +49,29 @@ function App() {
       <section className='content'>
         <div className='tasks'>
           <div className='tasks__div' >
-            <input className='tasks__input' placeholder='Введите текст' />
-            <button className='tasks__send'>Send</button>
+            <input 
+              className='tasks__input' 
+              placeholder='Введите текст' 
+              value={value} 
+              onChange={onChange}
+              />
+            <button type='submit' className='tasks__send' onClick={sendBtn} >Send</button>
           </div>
-          <div className='task'>
-            <p className='task__text'>Текст задачи</p>
-            <button className='task__btn task__btn_edit' />
-            <button className='task__btn task__btn_completed' />
-            <button className='task__btn tast__btn_delete' />
-          </div>
+          {card ?  <div key={count++} id={count++}>
+            <div className='task'>
+              <p className={`task__text ${check ? 'check' : ''}`}>{taskText}</p>
+              <button type='button' className='task__btn task__btn_edit' onClick={editBtn}/>
+              <button type='button' className='task__btn task__btn_completed' onClick={toggleCheckBtn}/>
+              <button type='reset' className='task__btn tast__btn_delete' />
+            </div>
+          </div> : ''
+          }
+          {/* <div className='task'>
+            <p className={`task__text ${check ? 'check' : ''}`}>{taskText}</p>
+            <button type='button' className='task__btn task__btn_edit' onClick={editBtn}/>
+            <button type='button' className='task__btn task__btn_completed' onClick={toggleCheckBtn}/>
+            <button type='reset' className='task__btn tast__btn_delete' />
+          </div> */}
         </div>
       </section>
     </div>
