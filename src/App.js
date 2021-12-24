e17c8d8e25c9c510be2f79df1e5eac0eca45115e
Nikita Chernov <ns4ern@gmail.com>
2021-12-24 04:25:57 +0300

    final html

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ function App() {
         <div className='tasks'>
           <div className='tasks__div' >
             <input className='tasks__input' placeholder='Введите текст' />
-            <button className='task__btn tasks__send'>Send</button>
+            <button className='tasks__send'>Send</button>
           </div>
           <div className='task'>
             <p className='task__text'>Текст задачи</p>
             <button className='task__btn task__btn_edit' />
-            <button className='task__btn tast__btn_delete'>Delete</button>
+            <button className='task__btn task__btn_completed' />
+            <button className='task__btn tast__btn_delete' />
           </div>
         </div>
       </section>
